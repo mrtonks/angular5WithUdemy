840e5da41249aefc15423501f048d3e2abba4575
Jesus Vera <yue.vera@gmail.com>
2018-02-18 16:27:20 -0600

    first commit

--- basics-assignment-1-start/src/app/app.component.ts
+++ basics-assignment-1-start/src/app/app.component.ts
@@ -1,15 +1,9 @@
-// Business logic
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
