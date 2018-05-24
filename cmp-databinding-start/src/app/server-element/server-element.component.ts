55a1272a109c7f3dbeaae1e3aff7d148a7632270
Jesus Vera <yue.vera@gmail.com>
2018-05-23 21:42:11 -0500

    Add lifecycle examples

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  DoCheck,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,40 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent 
+  implements OnInit, OnChanges, DoCheck, OnDestroy, AfterViewInit, AfterContentInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    console.log('ngOnChange called!');
   }
 
+  ngOnInit() {
+    console.log('ngOnInit Called!');
+    console.log('Text Content:', this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph:', this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content:', this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoChecked Called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph:', this.paragraph.nativeElement.textContent);
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
 }
