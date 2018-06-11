3ddf38e4ed64d6a07697455fc68ab08e0d160372
Jesus Vera <yue.vera@gmail.com>
2018-06-10 20:15:17 -0500

    master-chef: Adding navigation

--- master-chef/src/app/header/header.component.ts
+++ master-chef/src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() menuOptionSelected = new EventEmitter<Number>();
 
+    onSelect(optionSelected : number) {
+        this.menuOptionSelected.emit(optionSelected);
+    }
 }
